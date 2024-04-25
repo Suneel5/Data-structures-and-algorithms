5f9f06ebbcc82ddfacdcdce2cd542d3addabff1f
sunil <sunnelpanday9@gmail.com>
2024-04-25 08:33:28 +0545

    insertino-sort

--- sorting/insertion_sort.c
+++ sorting/insertion_sort.c
@@ -17,13 +17,14 @@ void insertion_sort(int arr[], int n) {
 }
 
 int main() {
+    int i;
     int arr[] = {0, 4, 3, 6, 1, 9, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     insertion_sort(arr, n);
 
     printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
+    for ( i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
